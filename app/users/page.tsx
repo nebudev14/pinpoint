04306738fa87abcae915c1304a1a5d8842897736
Emily Yin <emilyyin2020@gmail.com>
2024-10-05 12:53:55 -0400

    working with users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,8 @@
 import { createClient } from '@/utils/supabase/server';
 
-export default async function Notes() {
+export default async function Users() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: notes } = await supabase.from("users").select();
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
